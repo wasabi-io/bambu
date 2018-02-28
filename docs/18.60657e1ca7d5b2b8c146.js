018ecd0d880e075d7f3e41be38ce6dbb85785bb7
Aras <recepkoseoglu@hotmail.com>
2018-03-01 01:15:24 +0300

    documantation updated

---
+++ docs/18.60657e1ca7d5b2b8c146.js
@@ -0,0 +1,23 @@
+webpackJsonp([18],{
+
+/***/ 115:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = __webpack_require__(0);
+const Stateless_1 = __webpack_require__(13);
+const Page_1 = __webpack_require__(30);
+class Panel extends Stateless_1.default {
+    render() {
+        return (React.createElement(Page_1.default, { title: "Panel", subTitle: "A composable panel, for compact controls", hasMeta: true, variables: true }));
+    }
+}
+exports.default = Panel;
+
+
+/***/ })
+
+});
+//# sourceMappingURL=18.60657e1ca7d5b2b8c146.js.map
