88d7063b8357f865716c3d8e4cfc76d6f4e5d981
Aras <recepkoseoglu@hotmail.com>
2018-03-04 00:11:06 +0300

    added element of icon to showcase

--- src/elements/icon/Icon.tsx
+++ src/elements/icon/Icon.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { has } from 'wasabi-common';
 import JSXUtil from 'wasabi-ui/lib/jsx/JSXUtil';
 
+import { Color } from '../../base/css';
 import HTMLComponent, { HTMLSpanProps } from '../../base/html/HTML';
 import FaIcon, { FaIconProps, IconSize, IconSizeValues } from './FaIcon';
 import IconStyle from './IconStyle';
@@ -16,12 +17,14 @@ export type IconOptions = string | FaIconProps | JSX.Element;
 export interface IconProps extends HTMLSpanProps {
   icon: string | IconOptions;
   size?: string | IconSize;
+  color?: string | Color;
 }
 
 export default class Icon extends HTMLComponent<IconProps> {
   public static propTypes = {
     ...HTMLComponent.propTypes,
     icon: PropTypes.any,
+    color: PropTypes.string,
     size: PropTypes.oneOf(IconSizeValues),
   };
 
@@ -44,15 +47,16 @@ export default class Icon extends HTMLComponent<IconProps> {
   }
 
   public render() {
-    const { size, icon, className, ...props } = this.props;
+    const { size, icon, color, className, ...props } = this.props;
 
     const classNames = ClassNames(
       IconStyle.icon,
+      IconStyle[color],
       className
     );
 
     return (
-      <span className={IconStyle.icon} {...props} >
+      <span className={classNames} {...props} >
         {Icon.renderIcon(icon, size)}
       </span>
     );
