4ffb40cb81ac6ba5207e5af3a45dc6053e536657
Aras <recepkoseoglu@hotmail.com>
2018-03-04 02:33:48 +0300

    documantation updated

---
+++ docs/22.1f6e14b93026e84443d2.js
@@ -0,0 +1,23 @@
+webpackJsonp([22],{
+
+/***/ 115:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = __webpack_require__(0);
+const Stateless_1 = __webpack_require__(13);
+const Page_1 = __webpack_require__(30);
+class Panel extends Stateless_1.default {
+    render() {
+        return (React.createElement(Page_1.default, { title: "Panel", subTitle: "A composable panel, for compact controls", hasMeta: true, variables: true }));
+    }
+}
+exports.default = Panel;
+
+
+/***/ })
+
+});
+//# sourceMappingURL=22.1f6e14b93026e84443d2.js.map
---
+++ docs/23.1f6e14b93026e84443d2.js
@@ -0,0 +1,23 @@
+webpackJsonp([23],{
+
+/***/ 114:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = __webpack_require__(0);
+const Stateless_1 = __webpack_require__(13);
+const Page_1 = __webpack_require__(30);
+class Pagination extends Stateless_1.default {
+    render() {
+        return (React.createElement(Page_1.default, { title: "Pagination", subTitle: "A responsive, usable, and flexible paginations", hasMeta: true, variables: true, sizes: true }));
+    }
+}
+exports.default = Pagination;
+
+
+/***/ })
+
+});
+//# sourceMappingURL=23.1f6e14b93026e84443d2.js.map
