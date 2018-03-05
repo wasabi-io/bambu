d3ce6771babbd7e667ff5f085ba1c35c78287a52
Aras <recepkoseoglu@hotmail.com>
2018-03-05 15:11:10 +0300

    documantation updated

---
+++ docs/24.4185351376a517e8fbec.js
@@ -0,0 +1,23 @@
+webpackJsonp([24],{
+
+/***/ 115:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = __webpack_require__(0);
+const Stateless_1 = __webpack_require__(13);
+const Page_1 = __webpack_require__(30);
+class Pagination extends Stateless_1.default {
+    render() {
+        return (React.createElement(Page_1.default, { title: "Pagination", subTitle: "A responsive, usable, and flexible paginations", hasMeta: true, variables: true, sizes: true }));
+    }
+}
+exports.default = Pagination;
+
+
+/***/ })
+
+});
+//# sourceMappingURL=24.4185351376a517e8fbec.js.map
---
+++ docs/25.4185351376a517e8fbec.js
@@ -0,0 +1,23 @@
+webpackJsonp([25],{
+
+/***/ 114:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = __webpack_require__(0);
+const Stateless_1 = __webpack_require__(13);
+const Page_1 = __webpack_require__(30);
+class Navbar extends Stateless_1.default {
+    render() {
+        return (React.createElement(Page_1.default, { title: "Navbar", subTitle: "A responsive horizontal navbar that can support images, links, buttons, and dropdowns", hasMeta: true, variables: true, colors: true }));
+    }
+}
+exports.default = Navbar;
+
+
+/***/ })
+
+});
+//# sourceMappingURL=25.4185351376a517e8fbec.js.map
